f0ca4b2ab84262e933536ca8c0bd394929e8ee9d
Laoujin <woutervs@hotmail.com>
2015-01-30 02:34:12 +0100

    ex 2 - simulate multiple inputs

--- kata-2/test_kata2_hard.py
+++ kata-2/test_kata2_hard.py
@@ -14,6 +14,8 @@ import scalc
 
 ########################## HARD KATA-2 TESTS
 
+input_index = -1
+
 def setup_function(function):
 	backup = sys.stdout
 	f = io.StringIO()
@@ -33,6 +35,7 @@ def test_result_also_to_stdout():
 	assert total == int(out)
 
 # ex 1b
+
 def test_scalc_cli_with_input():
 	scalc.get_input = lambda x: ""
 	scalc.main("1,2,3")
@@ -41,3 +44,19 @@ def test_scalc_cli_with_input():
 
 	assert "6" in out
 
+# ex 2
+def input_replacer(text):
+	global input_index
+
+	input_index += 1
+	return ["1,2", "5", ""][input_index]
+
+def test_scalc_cli_with_multiple_input():
+	scalc.get_input = input_replacer
+	scalc.main("1,2,3")
+
+	out = sys.stdout.getvalue()
+
+	assert "6" in out
+	assert "3" in out
+	assert "5" in out
