78abe91bffee24e34e7ded51ff4c791490360a8d
Laoujin <woutervs@hotmail.com>
2015-01-30 02:21:51 +0100

    ex 2 - user input

--- kata-2/scalc.py
+++ kata-2/scalc.py
@@ -7,11 +7,25 @@ import sys
 
 from Calculator import Calculator
 
-def main(*files):
-	print("The result is ", end="")
+def get_input(text):
+	print(text, end="")
+	return input()
 
+def calc_and_print(numbers):
+	print("The result is ", end="")
 	calcer = Calculator()
-	total = calcer.add(files[0])
+	total = calcer.add(numbers)
+
+def main(*files):
+	calc_and_print(files[0])
+	while True:
+		numbers = get_input("another input please: ")
+		if numbers == "":
+			break
+
+		calc_and_print(numbers)
+
+
 
 if __name__ == "__main__":
     try:
--- kata-2/test_kata2_hard.py
+++ kata-2/test_kata2_hard.py
@@ -33,9 +33,11 @@ def test_result_also_to_stdout():
 	assert total == int(out)
 
 # ex 1b
-def test_scalc_cli():
+def test_scalc_cli_with_input():
+	scalc.get_input = lambda x: ""
 	scalc.main("1,2,3")
 
 	out = sys.stdout.getvalue()
 
-	assert out.rstrip().endswith("6")
+	assert "6" in out
+
